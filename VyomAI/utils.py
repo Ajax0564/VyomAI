018a2cdafc636f11f412eb08c4181680f27ce234
Ajax0564 <ankit51200iit@gmail.com>
2024-03-30 19:14:33 +0530

    added model size calculator

--- VyomAI/utils.py
+++ VyomAI/utils.py
@@ -1,1 +1,14 @@
 #for model making utils
+import torch.nn as nn
+
+
+def model_size(model: nn.Module) -> float:
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+
+    size_all_mb = (param_size + buffer_size) / 1024**2
+    return size_all_mb
