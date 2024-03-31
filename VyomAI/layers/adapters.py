e1503ee572a0d33411019a19231791b571a6586c
Ajax0564 <ankit51200iit@gmail.com>
2024-04-01 00:21:02 +0530

    added LoRA layer

---
+++ VyomAI/layers/adapters.py
@@ -0,0 +1,48 @@
+# To Do
+#add Lora and Dora
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+class MergeLoRALinear(nn.Module):
+    def __init__(self, linear_layer, rank, alpha, lora_dropout=0.):
+        super().__init__()
+        
+        self.linear = linear_layer
+        
+        self.in_features = self.linear.in_features
+        self.out_features = self.linear.out_features
+        self.rank = rank
+        self.alpha = alpha
+        
+        std_dev = 1 / torch.sqrt(torch.tensor(self.rank).float())
+        
+        self.A = nn.Parameter(torch.randn(self.rank,self.in_features,) * std_dev)
+        self.B = nn.Parameter(torch.zeros(self.out_features,self.rank)) #reverse  order due to F.linear as it process xA^T
+        self.dropout = nn.Dropout(lora_dropout)
+        
+    def forward(self, x):
+        
+        linear_output = self.linear(x)
+        lora_output = self.alpha * F.linear(F.linear(x,self.A,bias=None),self.B)      #(x @ self.A @ self.B)
+        lora_output = self.dropout(lora_output)
+        return linear_output+lora_output
+
+# lora_config = {
+#     'rank': 4,
+#     'alpha': 8,
+#     'lora_dropout': 0.05,
+#     'modules': ['query_proj','value_proj']
+# }
+# apply_lora = partial(
+#     LoRALinear,
+#     rank=lora_config['rank'],
+#     alpha=lora_config['alpha'],
+#     lora_dropout=lora_config['lora_dropout']
+# )
+#     for p in model.parameters():
+#     p.requires_grad = False
+
+# for layer in model.deberta.encoder.layer:
+#     if 'query_proj' in lora_config['modules']:
+#         layer.attention.self.query_proj = apply_lora(layer.attention.self.query_proj)
